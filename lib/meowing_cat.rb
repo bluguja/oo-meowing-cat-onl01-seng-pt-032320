b38c5af44c716d6134c4ac084892c4e001a7f46f
Bala Luguja <bluguja@gmail.com>
2020-04-20 17:24:02 +0000

    Automatically backed up by Learn

--- lib/meowing_cat.rb
+++ lib/meowing_cat.rb
@@ -1,1 +1,26 @@
-## code your solution here. 
+class Cat
+  attr_accessor:name
+  
+  def initialize(name)
+    @name = name
+    
+  end
+  
+  def name=(name)
+    @name = name
+  end
+  
+  def name
+    @name
+  end
+  
+  def speak
+    puts "Meow!"
+  end
+
+end
+
+
+
+
+  
